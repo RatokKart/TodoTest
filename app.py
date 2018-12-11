3a7bfccf2474c4d6217d9bf55b25e79d7b9419dd
RatokKart <hiro.sacredstar2@gmail.com>
2018-12-12 00:45:29 +0900

    GET実験中

--- app.py
+++ app.py
@@ -9,28 +9,32 @@ app.debug = True
 
 
 @app.route('/')
-def index():
+def top():
     return u'テスト'
 
 #初期表示
 @app.route('/top/<name>')
-def hello(name=''):
+def index(name=''):
     return render_template('index.html', name=name)
 
 #検索
-@app.route('/search')
+@app.route('/search', methods=['GET','POST'])
+#@app.route('/search')
 def search():
-    dt = request.args.get('text', '')
 
-    ddd = dt
+    if request.method == 'GET':
+        res = request.args.get('get_value')
+    elif request.method == 'POST':
+        res = request.form['post_value']
+
+    dd = res
 
 
 
+    return render_template('index.html')
 
 
-
-
-#おまじない
+#おまじない（最下部に書くこと）
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(port=port)
